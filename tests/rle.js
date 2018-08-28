a20e43ff2c18aa2f2d4d55836c4203015f181b08
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-08-28 23:59:02 +0300

    some additional tests added

--- tests/rle.js
+++ tests/rle.js
@@ -6,4 +6,16 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 	});
+
+	QUnit.test('расширенный тест rle пройден', function (assert) {
+		let strings = {
+			'a': 'a',
+			'abcdef': 'abcdef',
+			'aabbbcccdbbdddbkkk': 'a2b3c3db2d3bk3',
+			'mfiiqkkeeeklllee': 'mfi2qk2e3kl3e2',
+			'kkkKKKKKffffiiiiYYeehekvm': 'k3K5f4i4Y2e2hekvm',
+			'ffffffffffdddddddddddjjjjjjjjjjrrrrrrr': 'f10d11j10r7'
+		}
+		for (let key in strings) assert.strictEqual(rle(key), strings[key])
+	});
 });
